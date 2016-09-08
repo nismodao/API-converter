4802a796758bc8c20d7a11ffc436b771058ea9ea
Nam Dao <nam.d.dao@gmail.com>
2016-09-08 10:28:35 -0700

    7 tests passing for vehicle endpoint

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,11 @@
 const expect = require('chai').expect;
 const request = require('request');
 
-describe ('Vehicle Info', () => {
+describe ('Vehicle Info endpoint', () => {
 
   it('/vehicles/:id should exist', (done) => {
     var id = 1234;
-    request.get({url: 'http://fuf.me:3000/vehicles/' + id},
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
     (err, res, body) => {
       if (!err) {
         expect(res.req.path).to.equal('/vehicles/1234');
@@ -19,7 +19,7 @@ describe ('Vehicle Info', () => {
 
   it('/vehicles/:id should have GET method', (done) => {
     var id = 1234;
-    request.get({url: 'http://fuf.me:3000/vehicles/' + id},
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
     (err, res, body) => {
       if (!err) {
         expect(res.req.method).to.equal('GET');
@@ -33,7 +33,7 @@ describe ('Vehicle Info', () => {
 
   it('/vehicles/:id should return a JSON string', (done) => {
     var id = 1234;
-    request.get({url: 'http://fuf.me:3000/vehicles/' + id},
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
     (err, res, body) => {
       if (!err) {
         expect(typeof JSON.parse(body)).to.equal('object');
@@ -43,6 +43,62 @@ describe ('Vehicle Info', () => {
         done();        
       }
     });    
-  })
+  });
+
+  it('/vehicles/:id should return vin key with string value', (done) => {
+    var id = 1234;
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
+    (err, res, body) => {
+      if (!err) {
+        expect(typeof JSON.parse(body)).to.equal('object');
+        done();  
+      } else if (err) {
+        console.log(err);
+        done();        
+      }
+    });    
+  });
+
+  it('/vehicles/:id should return color key with string value', (done) => {
+    var id = 1234;
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
+    (err, res, body) => {
+      if (!err) {
+        expect(typeof JSON.parse(body).vin).to.equal('string');
+        done();  
+      } else if (err) {
+        console.log(err);
+        done();        
+      }
+    });    
+  });
+
+  it('/vehicles/:id should return doorCount with number value', (done) => {
+    var id = 1234;
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
+    (err, res, body) => {
+      if (!err) {
+        expect(typeof JSON.parse(body).doorCount).to.equal('number');
+        done();  
+      } else if (err) {
+        console.log(err);
+        done();        
+      }
+    });    
+  });
+
+  it('/vehicles/:id should return driveTrain key with string value', (done) => {
+    var id = 1234;
+    request.get({url: 'http://localhost:3000/vehicles/' + id},
+    (err, res, body) => {
+      if (!err) {
+        expect(typeof JSON.parse(body).driveTrain).to.equal('string');
+        done();  
+      } else if (err) {
+        console.log(err);
+        done();        
+      }
+    });    
+  });
 }); 
 
